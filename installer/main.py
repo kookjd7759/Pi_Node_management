896318aa82a3f376622043897a4d0f5b7cfaca6c
국동균 <kookjd7759@naver.com>
2026-02-12 10:00:10 +0900

    update

--- installer/main.py
+++ installer/main.py
@@ -1,11 +1,17 @@
 import requests
+import os
+
 import tkinter as tk
 from tkinter import filedialog
 from urllib.parse import urlparse
 from pathlib import Path
 
-BASE_PATH = ''
+SORUCE_BASE = 'https://raw.githubusercontent.com/kookjd7759/Pi_Node_management/refs/heads/main/image/data'
+EXEFILE_PATH = 'https://raw.githubusercontent.com/kookjd7759/Pi_Node_management/main/dist/UI.exe'
+
+TARGET_BASE = ''
 IMAGE_BASE = ''
+DATA_BASE = ''
 
 def download_file(url: str, target: str):
     response = requests.get(url)
@@ -22,9 +28,24 @@ def _find_folder():
 
 if __name__ == '__main__':
     ### get base path from user
-    BASE_PATH = _find_folder()
-    print(f'selected target folder = {BASE_PATH}')
-    IMAGE_BASE += BASE_PATH + '\\image'
+    TARGET_BASE = _find_folder()
+    print(f'selected target folder = {TARGET_BASE}')
 
-    ### test
-    download_file('https://raw.githubusercontent.com/kookjd7759/Pi_Node_management/refs/heads/main/image/data/Pi_App_btn.png', BASE_PATH)
+    ### create folder
+    TARGET_BASE += '\\Pi Manager'
+    IMAGE_BASE += TARGET_BASE + '\\image'
+    DATA_BASE = IMAGE_BASE + '\\data'
+    os.makedirs(TARGET_BASE, exist_ok=True)
+    os.makedirs(IMAGE_BASE, exist_ok=True)
+    os.makedirs(DATA_BASE, exist_ok=True)
+
+    ### download image
+    image_list = ['Pi_App_btn.png',
+                  'basic_mining_rate_txt.png',
+                  'close_btn.png',
+                  'node_bonus_txt.png',
+                  'start_mining_txt.png',
+                  'status_btn.png']
+    for image in image_list:
+        download_file(SORUCE_BASE + f'/{image}', DATA_BASE)
+    download_file(EXEFILE_PATH, TARGET_BASE)
